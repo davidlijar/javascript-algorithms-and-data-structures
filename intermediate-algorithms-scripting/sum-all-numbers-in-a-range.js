100422a3f938623e8b523323cc5b9caea2ca6ae5
davidlijar <davidbeljar@gmail.com>
2023-05-21 20:42:16 +0630

    new js file

---
+++ intermediate-algorithms-scripting/sum-all-numbers-in-a-range.js
@@ -0,0 +1,15 @@
+function sumAll(arr) {
+  let sorted = arr.sort((a, b) => {
+    return a - b
+  })
+  let sum = 0
+
+  for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
+    sum += i
+  }
+
+  console.log(sum)
+  return sum
+}
+
+sumAll([4, 2])
