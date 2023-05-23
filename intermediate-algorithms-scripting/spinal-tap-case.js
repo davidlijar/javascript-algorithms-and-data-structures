f5b6fc3de990f01bfbba96ac3727a706eb08ee09
davidlijar <davidbeljar@gmail.com>
2023-05-23 06:30:12 +0630

    changes

--- intermediate-algorithms-scripting/spinal-tap-case.js
+++ intermediate-algorithms-scripting/spinal-tap-case.js
@@ -1,11 +1,15 @@
 function spinalCase(str) {
-  let regex = /[A-Z][a-z]+|[a-z]+/g;
-  let strArr = str.match(regex);
+  let regex = /[A-Z][a-z]+|[a-z]+/g
+  let strArr = str.match(regex)
 
-  strArr = strArr.map(item => item.toLowerCase());
-  strArr = strArr.join("-");
-  console.log(strArr);
-  return strArr;
+  strArr = strArr.map((item) => item.toLowerCase())
+  strArr = strArr.join('-')
+  console.log(strArr)
+  return strArr
 }
 
-spinalCase('AllThe-small Things');
+spinalCase('AllThe-small Things')
+spinalCase('This Is Spinal Tap')
+spinalCase('thisIsSpinalTap')
+spinalCase('The_Andy_Griffith_Show')
+spinalCase('Teletubbies say Eh-oh')
