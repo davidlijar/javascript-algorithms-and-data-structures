ecf5d0ce28d8a6668e7a8d5752c6191f6561ffe8
davidlijar <davidbeljar@gmail.com>
2023-05-23 08:23:07 +0630

    add message

--- intermediate-algorithms-scripting/search-and-replace.js
+++ intermediate-algorithms-scripting/search-and-replace.js
@@ -20,3 +20,26 @@ myReplace('I think we should look up there', 'up', 'Down')
 myReplace('This has a spellngi error', 'spellngi', 'spelling')
 myReplace('His name is Tom', 'Tom', 'john')
 myReplace('Let us get back to more Coding', 'Coding', 'algorithms')
+
+//below is freeCodeCamp's solution code
+/*
+function myReplace(str, before, after) {
+  // Find index where before is on string
+  var index = str.indexOf(before);
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    // Change the after word to be uncapitalized before we use it.
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  // Now replace the original str with the edited one.
+  str = str.replace(before, after);
+
+  return str;
+}
+
+// test here
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+*/
