d3bfc8006973af419f73022441fea50cd70e50c1
davidlijar <davidbeljar@gmail.com>
2023-05-25 08:51:59 +0630

    dna-paring js file

---
+++ intermediate-algorithms-scripting/dna-pairing.js
@@ -0,0 +1,31 @@
+function pairElement(str) {
+  let resultArr = []
+
+  for (let i = 0; i < str.length; i++) {
+    let tempArr = []
+
+    switch (str[i]) {
+      case 'G':
+        tempArr.push('G', 'C')
+        resultArr.push(tempArr)
+        break
+      case 'C':
+        tempArr.push('C', 'G')
+        resultArr.push(tempArr)
+        break
+      case 'A':
+        tempArr.push('A', 'T')
+        resultArr.push(tempArr)
+        break
+      case 'T':
+        tempArr.push('T', 'A')
+        resultArr.push(tempArr)
+        break
+    }
+  }
+
+  console.log(resultArr)
+  return resultArr
+}
+
+pairElement('GCGAT')
